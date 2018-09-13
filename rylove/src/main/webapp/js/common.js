4bc22bfae91dc076d6a59150fb0e601bf3f2509f
lcyyong <43041462+lcyyong@users.noreply.github.com>
2018-09-13 23:23:55 +0800

    Add files via upload

--- rylove/src/main/webapp/js/common.js
+++ rylove/src/main/webapp/js/common.js
@@ -42,24 +42,4 @@
     //    $('html').sakura();
     //});
 	
-	function submitRsvp(){
-		var myform = $('#rsvpForm');
-		var fd = new FormData(myform );
-		
-		$.ajax({
-			url: "/index",
-			data: fd,
-			cache: false,
-			processData: false,
-			contentType: false,
-			type: 'POST',
-			success: function (data) {
-				$('#result').html("Hello, " + data.name);
-				// do something with the result
-			}
-		});
-		
-		$.getJSON('/index', function(data) {
-		$('#result').html("Hello 2, " + data.name);
-});
-	}
+	
