969bd84ee4b07de2aba3e7035e06a9f4c1cba613
lcyyong <43041462+lcyyong@users.noreply.github.com>
2018-09-16 11:12:39 +0800

    revert

--- rylove/src/main/java/myapp/IndexAction.java
+++ rylove/src/main/java/myapp/IndexAction.java
@@ -17,8 +17,6 @@
 package myapp;
 
 import java.io.IOException;
-import java.util.Properties;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -39,52 +37,10 @@ public class IndexAction extends HttpServlet {
   public void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
 		  
-	  String fullName = (String)req.getParameter("fullName");
-	    System.out.println("----> post : " + fullName);
-		
-	    // Recipient's email ID needs to be mentioned.
-	    String to = "lcyyong@live.cn";
-
-	    // Sender's email ID needs to be mentioned
-	    String from = "ruiyonglove@gmail.com";
-
-	    // Assuming you are sending email from localhost
-	    String host = "localhost";
-
-	    // Get system properties
-	    Properties properties = System.getProperties();
-
-	    // Setup mail server
-	    properties.setProperty("mail.smtp.host", host);
-
-	    // Get the default Session object.
-	    Session session = Session.getDefaultInstance(properties);
-
-	    try {
-	       // Create a default MimeMessage object.
-	       MimeMessage message = new MimeMessage(session);
-
-	       // Set From: header field of the header.
-	       message.setFrom(new InternetAddress(from));
-
-	       // Set To: header field of the header.
-	       message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-	       // Set Subject: header field
-	       message.setSubject("This is the Subject Line!");
-
-	       // Now set the actual message
-	       message.setText("This is actual message");
-
-	       // Send message
-	       Transport.send(message);
-	       System.out.println("Sent message successfully....");
-	    } catch (MessagingException mex) {
-	       mex.printStackTrace();
-	    }
-	    
-	    
-		resp.setContentType("text/plain");
-	    resp.getWriter().println("{ \"name\": \""+fullName+"\" }");
+	String fullName = (String)req.getParameter("fullName");
+    System.out.println("----> post : " + fullName);
+	
+	resp.setContentType("text/plain");
+    resp.getWriter().println("{ \"name\": \""+fullName+"\" }");
   }
 }
